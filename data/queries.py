3e0fd2abeb20ef1e69ec4e1e94216f8a378a9676
iulian-coder <iulian.titei@gmail.com>
2020-03-21 10:22:43 +0200

    Add season card

--- data/queries.py
+++ data/queries.py
@@ -1,6 +1,16 @@
 from data import data_manager
 
 
+
+
+def get_20shows():
+    data = data_manager.execute_select(
+        f''' SELECT shows.id, shows.title, COUNT(seasons.show_id) as sezoane from shows INNER JOIN seasons on shows.id = seasons.show_id
+      WHERE shows.id = 1390
+    GROUP BY shows.id, shows.title
+    ;''')
+    return data
+
 def get_shows(page_number):
     data = data_manager.execute_select(
             f'''SELECT DISTINCT shows.id, shows.title, shows.year, shows.runtime, shows.trailer, shows.homepage, shows.rating, first_value(genres.name) OVER (PARTITION BY shows.id) 
@@ -14,12 +24,26 @@ def get_shows(page_number):
                                            ''')
     return data
 
+# Tv show Page Queries
 
 def tv_show(show_id):
     data =  data_manager.execute_select(
-            f'''SELECT {show_id}, shows.title, shows.trailer, seasons.title AS seasons
+            f'''SELECT shows.id, shows.title as name, shows.runtime, shows.year, shows.trailer, shows.overview, seasons.title
             FROM shows INNER JOIN seasons ON shows.id = seasons.show_id
-            LIMIT 15
+            WHERE shows.id = {show_id}
     ;''')
     return data
 
+def get_actors(show_id):
+    data = data_manager.execute_select(
+        f''' SELECT shows.id, actors.name FROM shows LEFT JOIN show_characters ON show_characters.show_id = shows.id LEFT JOIN actors ON actors.id = show_characters.actor_id
+    WHERE shows.id = {show_id}
+    ;''')
+    return data
+
+def get_genres(show_id):
+    data = data_manager.execute_select(
+        f''' SELECT shows.id, shows.title, g.name as genres FROM shows LEFT JOIN show_genres sg on shows.id = sg.show_id LEFT JOIN genres g on sg.genre_id = g.id
+      WHERE shows.id = {show_id}
+    ;''')
+    return data
--- main.py
+++ main.py
@@ -20,15 +20,23 @@ def index(page):
 def design():
     return render_template('design.html')
 
+@app.route('/test')
+def shows_20():
+    data = queries.genres()
+    print(data)
+    return render_template('test.html', data = data)
+
 @app.route('/tv-show/<show_id>')
 def view_show(show_id):
     data = queries.tv_show(show_id)
-    print(data)
+    actors = queries.get_actors(show_id)
+    genres = queries.get_genres(show_id)
+    print(genres)
     if data[0]['trailer']:
         trailer = data[0]['trailer'].replace('watch?v=', 'embed/', 1)
     else:
         trailer = ''
-    return render_template('tv-show.html', data=data, trailer=trailer)
+    return render_template('tv-show.html', data=data, trailer=trailer, actors=actors, genres=genres)
 
 
 def main():
